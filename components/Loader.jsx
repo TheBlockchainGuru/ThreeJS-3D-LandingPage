54149a04b83af42faf0b2c584b66b5cd2b2104ca
cryptodev26 <99428411+cryptodev26@users.noreply.github.com>
2022-07-19 15:06:06 -0400

    fix loader image position issue

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -4,8 +4,32 @@ import Image from "next/image";
 
 const Loader = () => (
   <Html>
-    <div style={{ position: 'relative', zIndex: '100', width: '100vw', height: '100vh', background: '#292929', left: '-50%', top: '-50vh' }}>
-        <Image src='/Assets/loader.gif' alt='pic' width={250} height={250} style={{ position: 'absolute', transform: 'translate3d(-50%, -50%, 0)', left: '50%', top: '50%' }} />
+    <div 
+      style={{ 
+        position: 'relative', 
+        zIndex: '100', 
+        width: '100vw', 
+        height: '100vh', 
+        background: '#292929', 
+        left: '-50%', 
+        top: '-50vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+        <Image 
+          src='/Assets/loader.gif' 
+          alt='pic' 
+          width={250} 
+          height={250} 
+          style={{ 
+            position: 'absolute', 
+            transform: 'translate3d(-50%, -50%, 0)', 
+            left: '50%', 
+            top: '50%' 
+          }} 
+        />
     </div>
   </Html>
 );
