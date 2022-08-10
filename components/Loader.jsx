4c90db0d3fb2cab9f25d54323af098732503e2ef
DreamWorker777 <110565432+DreamWorker777@users.noreply.github.com>
2022-08-09 22:25:48 -0400

    add loading percent

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,37 +1,68 @@
-import React from 'react';
-import { Html } from '@react-three/drei';
+import React, { useEffect, useRef, useState } from 'react';
+import { Html, useProgress } from '@react-three/drei';
 import Image from "next/image";
 
-const Loader = () => (
-  <Html>
-    <div 
-      style={{ 
-        position: 'relative', 
-        zIndex: '100', 
-        width: '100vw', 
-        height: '100vh', 
-        background: '#292929', 
-        left: '-50%', 
-        top: '-50vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
-    >
-        <Image 
-          src='/Assets/loader.gif' 
-          alt='pic' 
-          width={250} 
-          height={250} 
-          style={{ 
-            position: 'absolute', 
-            transform: 'translate3d(-50%, -50%, 0)', 
-            left: '50%', 
-            top: '50%' 
-          }} 
-        />
-    </div>
-  </Html>
-);
+const Loader = () => {
+	const { progress } = useProgress()
+
+	const [ percent, setPercent ] = useState(0)
+	const [ percentValue, setPercentValue ] = useState(0)
+
+	const percentValueRef = useRef()
+	percentValueRef.current = percentValue
+
+	useEffect(() => {
+		if( progress > percent ) {
+			setPercent(progress)
+		}
+	}, [progress])
+
+	useEffect(() => {
+		if( percentValue < percent ) {
+			setPercentValue(prev => prev + 1)
+		}
+	}, [percent])
+
+	useEffect(() => {
+		if( percentValue < percent ) {
+			setPercentValue(prev => prev + 1)
+		}
+	}, [percentValue])
+
+	return (
+		<Html>
+			<div 
+				style={{ 
+					position: 'relative', 
+					zIndex: '100', 
+					width: '100vw', 
+					height: '100vh', 
+					background: '#292929', 
+					left: '-50%', 
+					top: '-50vh',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					flexDirection: 'column',
+				}}
+			>
+				<Image 
+					src='/Assets/loader.gif' 
+					alt='pic' 
+					width={250} 
+					height={250} 
+					style={{ 
+						position: 'absolute', 
+						transform: 'translate3d(-50%, -50%, 0)', 
+						left: '50%', 
+						top: '50%' 
+					}} 
+				/>
+
+				<h1 style={{ color: '#e4f1ff' }}>{percentValue}%</h1>
+			</div>
+		</Html>
+	)
+}
 
 export default Loader;
